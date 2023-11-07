d8b4fc82a5d4cada0b1ccaa8e9198b92cdfd7d70
Dominik Furman <s222915@student.uek.krakw.pl>
2023-11-07 23:35:58 +0100

    project still in development alert added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// window.addEventListener('beforeunload', function (e) {
+//     // Cancel the event
+//     e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+//     // Chrome requires returnValue to be set
+//     e.returnValue = '';
+//   });
+window.alert('Portoflio Website still in development!')
+
 function showHome() {
     document.getElementById("home").style.display = "block";
     document.getElementById("about-me").style.display = "none";
